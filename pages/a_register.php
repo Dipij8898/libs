445c9f3e58815d4a143cf904e30bf9d449b16501
New_coder <71255183+new-programmer-tech@users.noreply.github.com>
2020-10-08 23:30:27 +0530

    admin regsitration is connected to backend

--- pages/a_register.php
+++ pages/a_register.php
@@ -1,3 +1,9 @@
+<?php
+
+include "connection.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang=en>
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
@@ -28,12 +34,15 @@
                         <p class="text-muted mb-0"> Join us to access our large collection of books   .</p>
                      </div>
                      <span class=clearfix></span>
-                     <form>
+
+                     <form name="a_register" action="" method="post">
                         <div class=form-group>
                            <label class=form-control-label>Name</label>
                            <div class="input-group input-group-merge">
+
                               <input type=text class="form-control form-control-prepend" id=input-name
-                                 placeholder="John Ive">
+                                 placeholder="John Ive" name="name">
+
                               <div class=input-group-prepend><span class=input-group-text><i
                                        data-feather=user></i></span></div>
                            </div>
@@ -41,8 +50,10 @@
                         <div class=form-group>
                            <label class=form-control-label>Mobile Number</label>
                            <div class="input-group input-group-merge">
+
                               <input type=tel class="form-control form-control-prepend" id=input-name
-                                 placeholder="+91 9999999999">
+                                 placeholder="+91 9999999999" name="mobile">
+
                               <div class=input-group-prepend><span class=input-group-text><i
                                        data-feather=phone></i></span></div>
                            </div>
@@ -50,22 +61,28 @@
                         <div class=form-group>
                            <label class=form-control-label>Email address</label>
                            <div class="input-group input-group-merge">
+
                               <input type=email class="form-control form-control-prepend" id=input-email
-                                 placeholder=name@example.com>
+                                 placeholder=name@example.com name="email">
+
                               <div class=input-group-prepend><span class=input-group-text><i
                                        data-feather=at-sign></i></span></div>
                            </div>
                         </div>
                         <div class="form-group mb-2">
                            <div class="d-flex align-items-center justify-content-between">
-                              <div><label class=form-control-label>Password</label></div>
+                              
+                              <div><label class=form-control-label name="password">Password</label></div>
+
                               <div class=mb-2><a href=# class="small text-muted text-underline--dashed border-primary"
                                     data-toggle=password-text data-target=#input-password>Show password</a>
                               </div>
                            </div>
                            <div class="input-group input-group-merge">
+
                               <input type=password class="form-control form-control-prepend" id=input-password
-                                 placeholder=********>
+                                 placeholder=******** name="password">
+
                               <div class=input-group-prepend><span class=input-group-text><i
                                        data-feather=key></i></span></div>
                            </div>
@@ -74,29 +91,93 @@
                            <div class="custom-control custom-checkbox mb-3"><input type=checkbox
                                  class=custom-control-input id=check-terms required> <label class=custom-control-label
                                  for=check-terms>I agree to the <a href=#>terms and conditions</a>
-                                    </label> </div> </div> <div class=mt-4><button type=submit
-                                       class="btn btn-block btn-primary">Create my
-                                       account</button>
-                           </div>
+
+
+                     </label> </div> </div> <div class=mt-4><button type=submit
+                      class="btn btn-block btn-primary" name="submit">Create my account</button>
+
+
+
+                     </div>
+
+
                      </form>
                      <div class="py-2 text-center"><span class="text-xs text-uppercase">or</span></div>
-                     <div class="text-center"><small>Already have an acocunt?</small> <a href="/pages/a_login.html"
-                           class="small font-weight-bold">Sign in</a></div>
+                     <div class="text-center"><small>Already have an account?</small> <a href="/pages/a_login.html"
+                           class="small font-weight-bold" >Sign in</a></div>
                   </div>
                </div>
             </div>
          </div>
       </div>
    </section>
-   <script src=../../assets/libs/jquery/dist/jquery.min.js> </script> <script
-      src=../../assets/libs/feather-icons/dist/feather.min.js> </script> <script src="/assets/js/main.js"> </script>
-   <script>
-      feather.replace({
-         width: "1em",
-         height: "1em"
-      })
-   </script>
 
+
+
+   <!-- -------------------------------------PHP-------------------------------------------------------  -->
+
+
+   <?php
+
+if(isset($_POST['submit']))
+
+{
+  $count=0;
+
+  $sql="SELECT name from `a_register`";
+  $res=mysqli_query($db,$sql);
+
+  while($row=mysqli_fetch_assoc($res))
+  {
+    if($row['name']==$_POST['name'])
+    {
+      $count=$count+1;
+    }
+  }
+
+
+  if($count==0)
+  {
+ mysqli_query($db,"INSERT INTO `a_register` VALUES('$_POST[name]', '$_POST[mobile]', '$_POST[email]', '$_POST[password]');");
+
+
+  ?>
+    
+    <?php
+  echo "<script> location.href='a_login.php'; </script>";
+  exit;
+   ?>
+   
+  <?php
+  }
+
+
+  else
+  {
+
+    ?>
+      <script type="text/javascript">
+        alert("The username already exist.");
+      </script>
+    <?php
+
+  }
+
+}
+
+?>
+
+<script src="../../libs/assets/libs/jquery/dist/jquery.min.js"> </script> <script
+        src="../../libs/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"> </script> <script
+        src="../../libs/assets/libs/feather-icons/dist/feather.min.js"> </script> <script
+        src="../../libs/assets/libs/fancyapps/jquery.fancybox.min.js"> </script> <script
+        src="../../libs/assets/js/main.js"> </script>
+    <script>
+        feather.replace({
+            width: "1em",
+            height: "1em"
+        })
+    </script>
 </body>
 
 </html>
--- pages/connection.php
+++ pages/connection.php
@@ -1,6 +1,15 @@
 <?php
 
 $db=mysqli_connect("localhost:3307","root","","library");  
-            /* server name, username, passwor, database name */
+ /* server name, username, password, database name */
+
+// if(!$db)
+// {
+//     die("connection failed:".mysqli_connect_error());
+
+// }
+
+// echo "connected";
+           
 
 ?>
